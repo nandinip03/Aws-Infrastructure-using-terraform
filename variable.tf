9c90470adcdab7e97dae60b5183db69fcd62e72f
Nandini Patil <patilnandini152@gmail.com>
2026-08-05 17:46:10 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "terraform-aws-project"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "password123"
+}
